fa73b3f7f6df2b330ef79416663e34cf8e2c709d
sidewinder040 <macevic040@hotmail.co.uk>
2020-09-18 22:45:33 +0100

    Refactored main todo instantiation

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 #include "ToDo.h"
 
 int main(int argc, char* argv[]) {
-    ToDo newItem;
-    newItem.SetTitle ("My Item");
-    newItem.SetDescription("Describing my item");
-    newItem.SetID (1);
-    newItem.SetStatus(ToDoStatus::NOT_STARTED);
+    ToDo newItem(1, "My Item", 
+            "Describing my item",
+            ToDoStatus::NOT_STARTED);
+    // newItem.SetTitle ("My Item");
+    // newItem.SetDescription("Describing my item");
+    // newItem.SetID (1);
+    // newItem.SetStatus(ToDoStatus::NOT_STARTED);
     newItem.DisplayTodo();
 
     // Test overloaded constructor
